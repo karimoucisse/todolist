d320ca657989bd1935b9ff9f36c146ebfefe3f17
karimou cisse <karimou.cisse1@gmail.com>
2021-10-12 14:48:56 +0200

    add index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+var form = document.getElementById("form");
+var input = document.getElementById("input")
+    // On récupère l'élément sur lequel on veut détecter le clic
+ function Submit() {          // On écoute l'événement click
+                 // On change le contenu de notre élément pour afficher "C'est cliqué !"
+}
