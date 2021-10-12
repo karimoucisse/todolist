a930b939a70cc3c85ed7889c0a3fc4cabb54938f
karimou cisse <karimou.cisse1@gmail.com>
2021-10-12 15:33:25 +0200

    element list

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var form = document.getElementById("form");
 var input = document.getElementById("input")
+var list = document.getElementById("list")
+
     // On récupère l'élément sur lequel on veut détecter le clic
- function Submit() {          // On écoute l'événement click
-    alert(input.value)              // On change le contenu de notre élément pour afficher "C'est cliqué !"
+ function onTaskSubmit() {          // On écoute l'événement click
+    var listElementDiv = document.createElement("div");
+    listElementDiv.innerHTML = input.value
+    list.innerHTML= listElementDiv
+             // On change le contenu de notre élément pour afficher "C'est cliqué !"
 }
